69f965d8e3062e773219d90f0a574b52f4c2dc2d
dariomatias-dev <matiasdario75@gmail.com>
2026-05-24 17:07:46 -0300

    feat(app/layout.tsx): added header and footer
    - Implemented the header and footer components.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import type { ReactNode } from "react";
 
 import "./globals.css";
 
+import Footer from "@/components/layout/Footer";
+import Header from "@/components/layout/header/Header";
+
 export const metadata: Metadata = {
   title: "GoPkg",
   description:
@@ -13,7 +16,11 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className="bg-[#F8FAFC] text-slate-900 selection:bg-go-blue selection:text-white flex flex-col min-h-screen">
+        <Header />
+
         <main className="flex-1 flex flex-col">{children}</main>
+
+        <Footer />
       </body>
     </html>
   );
