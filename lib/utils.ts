916c0dc1737632bfa48b2828c89af827d475c821
dariomatias-dev <matiasdario75@gmail.com>
2026-05-25 18:59:41 -0300

    feat(lib/utils.ts): created import path encoding utility function
    - Developed utility function for import path encoding.

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function encodeImportPath(importPath: string): string {
+  return importPath.split("/").map(encodeURIComponent).join("/");
+}
